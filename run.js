ce0e092740120bc6c6daa0e9454345d519174fe5
stufie <yysophie10@gmail.com>
2020-10-20 22:25:02 -0700

    failed horribly

--- run.js
+++ run.js
@@ -24,10 +24,27 @@ client.on('message', async msg => {
 				return;
 			}
 			senderData.cooldowns.allowance = time;
-			let allowance = Math.floor(Math.random() * 30) + 30;
+			let allowance = Math.floor(Math.random() * 35) + 25;
 			senderData.balance += allowance;
 			msg.channel.send(`Your mom gave out candy and had ${allowance} candies left over for you\nHere, you can take it!`);
 		break;
+
+		case "boo!":
+		switch( ) {
+			case 1:
+			msg.channel.send("You scared the poor kid and they dropped 3 candies you ugly bastard!");
+			return;
+		}
+		senderData.cooldowns.boo = time;
+		senderData.balance += 3;
+			break;
+			case 2:
+			msg.channel.send("You tried to scare the kid but they jumped you and stole your candy! -3 candies");
+			return;
+		}
+		senderData.cooldowns.boo = time
+		senderData.balance -= 3;
+			break;
 
 		case "profile":
 			msg.channel.send(`You have ${senderData.balance} candies in your Trick O' Treat bag`);
@@ -37,11 +54,11 @@ client.on('message', async msg => {
 			msg.channel.send("I'm still alive. \nBut you won't be for long.");
 		break;
 
-		case "trick":
+		case "knock":
 			if(/^<@!\d+>$/.test(cmd.parsed[0])) {
 				let userToTrick = cmd.parsed[0].match(/(?<=^<@!)\d+(?=>$)/)[0];
 				badDatabase.get(userToTrick).trick += 1;
-				msg.channel.send(`You've set up a nasty trick for ${cmd.parsed[0]}`);
+				msg.channel.send(`You visited ${cmd.parsed[0]} house while you were trick or treating`);
 			}
 			else msg.channel.send(`Hmm, I can't find ${cmd.parsed[0]}'s address...`);
 		break;
