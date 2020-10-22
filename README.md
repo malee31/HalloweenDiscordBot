cd67799f64a296544da94de22edcd9dfa6698aa5
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-10-21 18:11:35 -0700

    Added a README.md and setup instructions and a setup script

---
+++ README.md
@@ -0,0 +1,13 @@
+# Halloween Discord Bot
+----------------------
+This Discord Bot was made for a Halloween event on Sophie's WHOLE Circus 🤹🎭🎪🌝🌚🤡 Discord Server.
+Collect Candies and participate in random events to be the best Trick O' Treater around!
+
+## Setup
+To set up the bot, set up the bad JSON database by running the `dbSetup.js` file in the `parts/` folder.</br>
+The setup script will also create a `.env` file at the project root which will have to contain the Discord Bot's token inside the quotes.</br>
+Once this is done, run `npm install` to install the dependencies required for the project and run `npm start` to turn the bot on.
+
+## Adjustments
+Small adjustments such as changing cooldown timers or prefixes can be done in the `config.json` file inside of the `parts/` folder.</br>
+All code for commands are in the `run.js` file and can be changed up there.
---
+++ parts/dbSetup.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+fs.access(`${__dirname}/../saveData.json`, fs.constants.F_OK | fs.constants.W_OK, (err) => {
+	if(err) {
+		if(err.code === 'ENOENT') {
+			fs.writeFile(`${__dirname}/../saveData.json`, '{"user": {}}', (err) => {
+				if(err) {
+					console.log(`Error trying to create saveData.json: \n${err}`);
+				}
+				console.log('Created saveData.json');
+			});
+		}
+	} else {
+		console.log('saveData.json File Already Exists');
+	}
+});
+
+fs.access(`${__dirname}/../.env`, fs.constants.F_OK | fs.constants.W_OK, (err) => {
+	if(err) {
+		if(err.code === 'ENOENT') {
+			fs.writeFile(`${__dirname}/../.env`, 'discordtoken="Insert your Discord Bot Token in these quotes"', (err) => {
+				if(err) {
+					console.log(`Error trying to create .env file: \n${err}`);
+				}
+				console.log('Created .env file');
+			});
+		}
+	} else {
+		console.log('.env File Already Exists');
+	}
+});
