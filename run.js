b9df23454b0e1d679c93006d60ae197d691adb1a
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-10-22 21:16:38 -0700

    Moved reaction and message response stuff into parts/eventHandler.js

--- run.js
+++ run.js
@@ -1,13 +1,10 @@
 const Discord = require("discord.js");
 const client = require("./parts/bot.js");
 const config = require("./parts/config.json");
-const cmdParse = require("./parts/commandParse.js");
 const argFormat = require("./parts/format.js");
+const cmdParse = require("./parts/commandParse.js");
 const badDatabase = require("./parts/badDatabase.js");
-
-let time = Math.floor(new Date().getTime() / 1000);
-let currentEvents = [];
-let eventTimer = setInterval(eventClear, config.eventTimerSpeed);
+const { eventsNow, forceStartEvent, randomEvent, cooldown } = require("./parts/randomEvent.js");
 
 client.on('message', async msg => {
 	if(msg.author.bot) return;
@@ -24,11 +21,10 @@ client.on('message', async msg => {
 			if(msg.member.hasPermission("ADMINISTRATOR")) {
 				forceStartEvent(msg.channel);
 			}
-				forceStartEvent(msg.channel);
 		break;
 
 		case "currentevents":
-			msg.channel.send(JSON.stringify(currentEvents));
+			msg.channel.send(JSON.stringify(eventsNow()));
 		break;
 
 		case "greet":
@@ -38,7 +34,7 @@ client.on('message', async msg => {
 		case "pass": 
 			remainingCooldown = cooldown("pass", senderData);
 			if(remainingCooldown !== -1) {
-				msg.channel.send(`If you take all the candy, your mom will get mad!\nCooldown: ${remainingCooldown}s left`);
+				msg.channel.send(`If you take all the candy, your mom will get mad!\nCooldown: ${remainingCooldown}`);
 				return;
 			}
 
@@ -50,7 +46,7 @@ client.on('message', async msg => {
 		case "boo":
 			remainingCooldown = cooldown("boo", senderData);
 			if(remainingCooldown !== -1) {
-				msg.channel.send(`All the kids have already been scared off. Now to wait...\nCooldown: ${remainingCooldown}s left`);
+				msg.channel.send(`All the kids have already been scared off. Now to wait...\nCooldown: ${remainingCooldown}`);
 				return;
 			}
 
@@ -111,7 +107,7 @@ client.on('message', async msg => {
 		case "trickotreat":
 			remainingCooldown = cooldown("trickotreat", senderData);
 			if(remainingCooldown !== -1) {
-				msg.channel.send(`"Walk to the next house! Stop running or we're never trick o' treating again!"\n-Your Mom\nCooldown: ${remainingCooldown}s left`);
+				msg.channel.send(`"Walk to the next house! Stop running or we're never trick o' treating again!"\n-Your Mom\nCooldown: ${remainingCooldown}`);
 				return;
 			}
 
@@ -149,90 +145,6 @@ client.on('message', async msg => {
 	randomEvent(msg.channel);
 });
 
-client.on('messageReactionAdd', (reaction, user) => {
-	//console.log(`${user.username} reacted with "${reaction.emoji.name}".`);
-	eventClear();
-	if(user.bot) return;
-	let eventLookup = currentEvents.find(events => events.id == reaction.message.id);
-	if(typeof eventLookup == "undefined") return;
-	switch(eventLookup.type) {
-		case "react":
-			let index = eventLookup.data.indexOf(reaction.emoji.name);
-			if(index == -1) return;
-			eventLookup.data.splice(index, 1);
-			badDatabase.get(user.id).balance += 10;
-			reaction.message.edit(`${reaction.message.content}\n${user.username}#${user.discriminator} got 10 ${reaction.emoji.name}`);
-			eventClear(eventLookup.id);
-		break;
-	}
-});
-
-client.on('messageReactionRemove', (reaction, user) => {
-	console.log(`${user.username} removed their "${reaction.emoji.name}" reaction.`);
-});
-
-function cooldown(cooldownName, senderData) {
-	if(typeof config.cooldowns[cooldowns] == "undefined") throw "No default cooldown in config";
-	if(time - senderData.cooldowns[cooldownName] < config.cooldowns[cooldownName]) {
-		return config.cooldowns[cooldownName] - (time - senderData.cooldowns[cooldownName]);
-	}
-
-	senderData.cooldowns[cooldownName] = time;
-	return -1;
-}
-
-function startEvent(eventObject) {
-	currentEvents.push(eventObject);
-}
-
-function eventClear(id) {
-	time = Math.floor(new Date().getTime() / 1000);
-	for(let eventNum = 0; eventNum < currentEvents.length; eventNum++) {
-		let thisEvent = currentEvents[eventNum];
-		if(typeof config.eventDurations[thisEvent.type] == "undefined") throw "No default event duration in config";
-		if(time >= thisEvent.start + config.eventDurations[thisEvent.type] || (typeof id !== "undefined" && thisEvent.id == id)) {
-			currentEvents.splice(eventNum, 1);
-			eventExpire(thisEvent);
-			eventNum--;
-		}
-	}
-}
-
-function eventExpire(expiredEvent) {
-	console.log(`Expired event ${expiredEvent.type}`);
-}
-
-function randomEvent(channel) {
-	if(Math.floor(Math.random() * 100 + 1) < config.eventBaseChance) forceStartEvent(channel);
-}
-
-function forceStartEvent(channel) {
-	switch(config.enabledEvents[Math.floor(Math.random() * config.enabledEvents.length)]) {
-
-		case "react":
-			channel.send("QUICK! PICK UP THE CANDY!!!").then(sentMsg => {
-				startEvent({type: "react", startTime: time, id: sentMsg.id, data: ["🍬", "🍫", "🍭", "🍪"]});
-				sentMsg.react("🍬");
-				sentMsg.react("🍫");
-				sentMsg.react("🍭");
-				sentMsg.react("🍪");
-			}).catch(console.error);
-		break;
-
-		case "witch":
-			channel.send("There is a Witch in your neighborhood that is passing out KING SIZED candy bars.\nType \"treat\" to visit and \"trick\" to ignore.").then(sentMsg => {
-				startEvent({type: "witch", startTime: time, channelId: sentMsg.channel.id, id: sentMsg.id, data: []});
-			});
-		break;
-
-		case "mystic":
-			channel.send('🔮"Hey!" A mysterious fortune teller beckons you towards them🔮\n"You poor innocent child, if you send the spirits an offering, they may do you a favor in return..."\nDo you dare approach her? (approach/run) <Cost: 50 candies>').then(sentMsg => {
-				startEvent({type: "mystic", startTime: time, channelId: sentMsg.channel.id, id: sentMsg.id, data: []});
-			});
-		break;
-
-	}
-}
 
 function keywordHandler(msg) {
 	let keyword = msg.content.trim().toLowerCase();
@@ -240,7 +152,7 @@ function keywordHandler(msg) {
 	switch(keyword) {
 		case "trick":
 		case "treat":
-			eventLookup = currentEvents.filter(events => events.type == "witch" && events.channelId == msg.channel.id);
+			eventLookup = eventsNow().filter(events => events.type == "witch" && events.channelId == msg.channel.id);
 			for(let witchEvent of eventLookup) {
 				if(witchEvent.data.includes(msg.author.id)) continue;
 
@@ -261,7 +173,7 @@ function keywordHandler(msg) {
 		break;
 		case "approach":
 		case "run":
-			eventLookup = currentEvents.filter(events => events.type == "mystic" && events.channelId == msg.channel.id);
+			eventLookup = eventsNow().filter(events => events.type == "mystic" && events.channelId == msg.channel.id);
 			for(let mysticEvent of eventLookup) {
 				if(mysticEvent.data.includes(msg.author.id)) continue;
 
