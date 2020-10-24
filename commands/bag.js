4b0d717b2aa5f68a334097f620a6985e744d3825
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-10-24 16:37:34 -0700

    bag command has a nice embed

--- commands/bag.js
+++ commands/bag.js
@@ -1,9 +1,16 @@
+const Discord = require("discord.js");
 const badDatabase = require("../parts/badDatabase.js");
 
 module.exports = {
 	name: 'bag',
 	description: 'Check how many candies you have',
 	execute(message) {
-		return message.channel.send(`You have ${badDatabase.get(message.author.id).balance} candies in your Trick o' Treat bag`);
+		let bagEmbed = new Discord.MessageEmbed()
+		.setColor('#FF7518')
+		.setTitle("Trick o' Treat Bag")
+		.setDescription(`You have ${badDatabase.get(message.author.id).balance} candies in your Trick o' Treat bag`)
+		.setImage("https://media1.tenor.com/images/38109369073474ad9da629afae0c06d2/tenor.gif")
+
+		return message.channel.send(bagEmbed);
 	},
 };
