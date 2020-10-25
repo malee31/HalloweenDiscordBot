327075f4c3b7e431952bdb5d5ae1a777da8bfee1
stufie <yysophie10@gmail.com>
2020-10-24 21:50:54 -0700

    updated scaling for knock

--- commands/knock.js
+++ commands/knock.js
@@ -14,12 +14,30 @@ module.exports = {
 		let userToTrick = badDatabase.get(args[0].match(/(?<=^<@!?)\d+(?=>$)/)[0]);
 		let exchange = 0;
 		let knockMsg = "";
-		if(rand < 0.9) {
-			exchange = 2;
+		if(rand < 0.65) {
+			if(userToTrick.balance > 1000) {
+			exchange = 5;
+			knockMsg = `You knocked on ${args[0]}'s door and their mom made them give you 5 pieces of candy`;
+			} else if(userToTrick.balance > 300) {
+			exchange = 2; 
 			knockMsg = `You knocked on ${args[0]}'s door and their mom made them give you 2 pieces of candy`;
+	    	}
 		} else {
-			exchange = 6;
-			knockMsg = `You knocked on ${args[0]}'s door but they weren't home.\nYou grabbed 6 pieces of candy from their Take One candy bucket!`;
+			if(userToTrick.balance > 2000) {
+			exchange = 45;
+			knockMsg = `You knocked on ${args[0]}'s door but they weren't home so you stole their ENTIRE candy bucket. +45 candies`;
+			} else if(userToTrick.balance > 1500) {
+			exchange = 32; 
+			knockMsg = `You knocked on ${args[0]}'s door but they weren't home so you stole their ENTIRE candy bucket. +32 candies`;
+	    	} else if(userToTrick.balance > 1000) {
+			exchange = 21;
+			knockMsg = `You knocked on ${args[0]}'s door but they weren't home so you stole their ENTIRE candy bucket. +21 candies`;
+			} else if(userToTrick.balance > 700) {
+			exchange = 15;
+			knockMsg = `You knocked on ${args[0]}'s door but they weren't home so you grabbed 15 pieces of candied in their take one candy bucket!`;
+			} else 
+			exchange = 7;
+			knockMsg = `You knocked on ${args[0]}'s door but they weren't home so you grabbed 7 pieces of candy from their Take One candy bucket!`;
 		}
 
 		badDatabase.get(message.author.id).balance += exchange;
