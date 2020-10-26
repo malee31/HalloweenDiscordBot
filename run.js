afb5ceab1dedbfe4480e63e5c8261b4a0ea278e4
Marv <43216761+malee31@users.noreply.github.com>
2020-10-25 18:58:34 -0700

    Add back share for a sec

--- run.js
+++ run.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./parts/config.json");
-const argFormat = require("./parts/format.js");
+// const argFormat = require("./parts/format.js");
 const cmdParse = require("./parts/commandParse.js");
 const {randomEvent} = require("./parts/randomEvent.js");
 
