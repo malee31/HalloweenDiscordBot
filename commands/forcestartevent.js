2dd83e55bb64f6b0c0e953decb25b0df9a9946cd
Marv <43216761+malee31@users.noreply.github.com>
2020-10-25 19:07:02 -0700

    Fix on admin only commands, whoops

--- commands/forcestartevent.js
+++ commands/forcestartevent.js
@@ -7,12 +7,13 @@ module.exports = {
 	execute(message, args) {
 		console.log(`Event request by: ${message.author.username}#${message.author.discriminator}`);
 		if(!message.member.hasPermission("ADMINISTRATOR") && message.author.id !== process.env.owner) {
-			message.reply(`You have no authority here. Kneel.`);
+			return message.reply(`You have no authority here. Kneel.`);
 		}
 		console.log(`Force starting event by: ${message.author.username}#${message.author.discriminator}`);
 		if(args[0]) {
 			console.log(args[0]);
 			forceStartEvent(message, args[0]);
+			return;
 		}
 		forceStartEvent(message);
 	},
